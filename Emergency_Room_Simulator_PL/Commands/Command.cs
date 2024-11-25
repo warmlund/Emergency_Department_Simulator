5f1679e5aa9d0edde02ae8a31c3a7220e6586a43
warmlund <emeliewarmlund@gmail.com>
2024-11-25 10:12:57 +0100

    Set up command classes and NotifyPropertyChanged Set up class view model

---
+++ Emergency_Room_Simulator_PL/Commands/Command.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Input;
+
+namespace Emergency_Department_Simulator_PL.Commands
+{
+    class Command : ICommand
+    {
+        private Action methodToExecute = null; //variable representing the command that is being executed
+        private Func<bool> canMethodBeExecuted = null; //variable that represents the boolean which checks if you can execute the command or not
+        public event EventHandler CanExecuteChanged;  // An event that is triggered if the state of _canExecute changes
+
+        /// <summary>
+        /// Constructor and sets the variable with the arguments
+        /// </summary>
+        public Command(Action methodToExecute, Func<bool> canMethodBeExecuted)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canMethodBeExecuted = canMethodBeExecuted;
+        }
+
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        public void Execute(object parameter)
+        {
+            methodToExecute?.Invoke();
+        }
+
+        /// <summary>
+        /// Method that determines if the command can be executed based on _canExecute
+        /// </summary>
+        public bool CanExecute(object parameter)
+        {
+            if (canMethodBeExecuted == null)
+            {
+                return true;
+            }
+            else
+            {
+                return canMethodBeExecuted();
+            }
+        }
+    }
---
+++ Emergency_Room_Simulator_PL/NotifyPropertyChanged.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Emergency_Department_Simulator_PL
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        //Event to notify if a proprety has changed
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the propertychanged event for a named property
+        /// This method is called if the property has changed
+        /// </summary>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
