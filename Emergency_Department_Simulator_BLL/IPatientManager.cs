06beb3ef30bda5b63fb78c4cf56f183fd9f816d9
warmlund <emeliewarmlund@gmail.com>
2024-11-26 10:02:45 +0100

    Structure for BLL layer

---
+++ Emergency_Department_Simulator_BLL/IPatientManager.cs
@@ -0,0 +1,17 @@
+﻿using Emergency_Department_Simulator_DTO;
+
+namespace Emergency_Department_Simulator_BLL
+{
+    internal interface IPatientManager
+    {
+        void AddPatient();
+        void UpdatePatient();
+        void LoadPatients();
+        void SavePatients();
+        void CreatePatientId();
+        int GetRegisteredPatients();
+        int GetDischargedPatients();
+        int GetTreatedPatients();
+        List<Patient> GetPatients();
+    }
+}
